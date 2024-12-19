a8eb382213d87c55dd09f94507eb1b60a693d402
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2024-12-19 17:45:07 -0300

    test: creating the first file for testing

---
+++ PaymentContext.Tests/Entities/StudantTests.cs
@@ -0,0 +1,12 @@
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities;
+
+[TestClass]
+public class StudantTests
+{
+    [TestMethod]
+    public void Teste1()
+    {
+    }
+}
